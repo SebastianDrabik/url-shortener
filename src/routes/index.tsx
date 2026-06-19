e3bd8110fdd3651e064a9c68d51a9f25e96025fe
SebastianDrabik <68112314+SebastianDrabik@users.noreply.github.com>
2026-06-19 20:13:31 +0200

    Add shadcn with custom theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {Button} from "#/components/ui/button.tsx";
+import {Card} from "#/components/ui/card.tsx";
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
+      <Card className="w-full max-w-2xl mx-auto">
+        <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
+        <p className="mb-4">This is a simple example of a home page using React Router and Tailwind CSS.</p>
+        <Button variant='default'>Click Me</Button>
+      </Card>
     </div>
   )
 }
