c95f2ad97cf3cc2a4b85cf002d6e32608fa7538b
SebastianDrabik <68112314+SebastianDrabik@users.noreply.github.com>
2026-06-30 23:51:14 +0200

    Add QR code to /check

--- src/routes/check/$url.tsx
+++ src/routes/check/$url.tsx
@@ -3,6 +3,7 @@ import {getShortLinkInfo, type PublicLink} from "#/functions/links.functions";
 import {PageTemplate} from "#/components/PageTemplate.tsx";
 import manQuestionMark from '@/assets/mq.svg'
 import womanRocket from '@/assets/wr.svg'
+import {QRCodeSVG} from "qrcode.react";
 
 export const Route = createFileRoute('/check/$url')({
     component: RouteComponent,
@@ -26,22 +27,20 @@ function LinkInfo({ link }: { link: PublicLink}) {
     return <div>
         <h2 className='text-2xl mb-2'>{location.origin}/<span className='text-accent-foreground font-bold text-3xl'>{link.shortUrl}</span></h2>
         <ul className='text-md list-disc pl-4'>
-            <li>Link status: {link.active ? 'active' : 'inactive'}</li>
-            <li>Creation date: {link.createdAt.toUTCString()}</li>
+            <li><span className='text-muted-foreground'>Link status:</span> {link.active ? 'active' : 'inactive'}</li>
+            <li><span className='text-muted-foreground'>Creation date:</span> {link.createdAt.toUTCString()}</li>
             {link.expiresAt && <li>Expiration date: {link.expiresAt.toUTCString()}</li>}
-            <li>QR TODO</li>
         </ul>
+
+        <div className='flex justify-center mt-4'>
+            <QRCodeSVG value={link.longUrl} level={'H'} size={200} fgColor="#1a1a1a" bgColor="#ffffff" imageSettings={{excavate: true, src: '/icon.svg', width: 50, height: 50}} />
+        </div>
     </div>
 }
 
 function LinkInfoNotFound() {
     return <div>
         <h2>{`${window.location.origin}/das`}</h2>
-        <ul className='list-disc pl-4'>
-            <li>Link status</li>
-            <li>Creation date</li>
-            <li>Expiration date</li>
-            <li>QR</li>
-        </ul>
+
     </div>
 }
